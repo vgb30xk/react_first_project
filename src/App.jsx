bb3bdea851e99b550078a9016cafa57c99ab4c0e
Jaehyun <vgb30xk@naver.com>
2022-12-05 15:25:03 +0900

    김유정->이범규 변경

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ const App = () => {
   const [users, setUsers] = useState([
     { id: 1, age: 30, name: "송중기" },
     { id: 2, age: 24, name: "송강" },
-    { id: 3, age: 21, name: "김유정" },
+    { id: 3, age: 21, name: "이범규" },
     { id: 4, age: 29, name: "구교환" },
   ]);
 
